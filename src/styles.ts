7362b2f2f5f139b45c8ba882754cf0cdca5abede
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-07-17 12:06:12 +0000

    Add styled component for ColumnContainer and ColumnTitle

--- src/styles.ts
+++ src/styles.ts
@@ -9,3 +9,18 @@ export const AppContainer = styled.div`
   background-color: #3179ba;
   padding: 20px;
 `;
+
+export const ColumnContainer = styled.div`
+  width: 300px;
+  min-height: 40px;
+  margin-right: 20px;
+  background-color: #ebecf0;
+  border-radius: 3px;
+  padding: 8px;
+  flex-grow: 0;
+`;
+
+export const ColumnTitle = styled.div`
+  padding: 6px 16px 12px;
+  font-weight: bold;
+`;
