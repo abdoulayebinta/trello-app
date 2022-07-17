88ac2972048f198011c021747abccfe1b68026a3
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-07-17 12:15:16 +0000

    Add styled component for the Card component

--- src/styles.ts
+++ src/styles.ts
@@ -24,3 +24,13 @@ export const ColumnTitle = styled.div`
   padding: 6px 16px 12px;
   font-weight: bold;
 `;
+
+export const CardContainer = styled.div`
+  max-width: 300px;
+  border-radius: 3px;
+  background-color: #fff;
+  cursor: pointer;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  box-shadow: #091e4240 0px 1px 0px 0px;
+`;
