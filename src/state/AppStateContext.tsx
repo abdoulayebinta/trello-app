9a77ffcce95a54ca08d2183cfd0aa07575a55e8c
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-07-23 16:01:24 +0000

    Define app satate with React ContextAPI

---
+++ src/state/AppStateContext.tsx
@@ -0,0 +1,59 @@
+import { useContext, createContext, FC } from 'react';
+
+type Task = {
+  id: string;
+  text: string;
+};
+
+type List = {
+  id: string;
+  text: string;
+  tasks: Task[];
+};
+
+export type AppState = {
+  lists: List[];
+};
+
+const appData: AppState = {
+  lists: [
+    {
+      id: '0',
+      text: 'To Do',
+      tasks: [{ id: 'c0', text: 'Generate app scaffold' }],
+    },
+    {
+      id: '1',
+      text: 'In Progress',
+      tasks: [{ id: 'c2', text: 'Learn TypeScript' }],
+    },
+    {
+      id: '2',
+      text: 'Done',
+      tasks: [{ id: 'c3', text: 'Begin to use static typing' }],
+    },
+  ],
+};
+
+type AppStateContextProps = {
+  lists: List[];
+  getTasksByListId(id: string): Task[];
+};
+
+const AppStateContext = createContext<AppStateContextProps>(
+  {} as AppStateContextProps
+);
+
+export const AppStateProvider: FC = ({ children }) => {
+  const { lists } = appData;
+
+  const getTasksByListId = (id: string) => {
+    return lists.find((list) => list.id === id)?.tasks || [];
+  };
+
+  return (
+    <AppStateContext.Provider value={{ lists, getTasksByListId }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
