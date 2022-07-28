fa515fdded2fbd0cac0a3a5c8c9814f3df6ecc2a
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-07-28 16:49:14 +0000

    Dispatch actions add task

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { AppContainer } from './styles';
 import { AddNewItem } from './components/AddNewItem';
 import { Column } from './components/Column';
 import { useAppState } from './state/appStateContext';
+import { addList, addTask } from './state/actions';
 
 export const App = () => {
-  const { lists } = useAppState();
+  const { lists, dispatch } = useAppState();
 
   return (
     <AppContainer>
       {lists.map((list) => (
         <Column title={list.text} id={list.id} key={list.id} />
       ))}
-      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+      <AddNewItem
+        toggleButtonText="+ Add another list"
+        onAdd={(text) => dispatch(addList(text))}
+      />
     </AppContainer>
   );
 };
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -2,6 +2,7 @@ import { ColumnContainer, ColumnTitle } from '../styles';
 import { Card } from './Card';
 import { AddNewItem } from './AddNewItem';
 import { useAppState } from '../state/appStateContext';
+import { addTask } from '../state/actions';
 
 type ColumnProps = {
   title: string;
@@ -9,7 +10,7 @@ type ColumnProps = {
 };
 
 export const Column = ({ title, id }: ColumnProps) => {
-  const { getTasksByListId } = useAppState();
+  const { getTasksByListId, dispatch } = useAppState();
 
   const tasks = getTasksByListId(id);
 
@@ -19,7 +20,10 @@ export const Column = ({ title, id }: ColumnProps) => {
       {tasks.map((task) => (
         <Card text={task.text} id={task.id} key={task.id} />
       ))}
-      <AddNewItem toggleButtonText="+ Add a card" onAdd={console.log} />
+      <AddNewItem
+        toggleButtonText="+ Add a card"
+        onAdd={(text) => dispatch(addTask(text, id))}
+      />
     </ColumnContainer>
   );
 };
