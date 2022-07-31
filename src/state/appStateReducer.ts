62cb794e181b85187d71ccb7b9f67a417e858bab
abdoulayebinta <bahabdoulayebinta@gmail.com>
2022-07-31 13:34:06 +0000

    Define Action and action creator for SET_DRAGGED_ITEM

--- src/state/appStateReducer.ts
+++ src/state/appStateReducer.ts
@@ -49,5 +49,9 @@ export const appStateReducer = (
       draft.lists = moveItem(draft.lists, dragIndex, hoverIndex);
       break;
     }
+    case 'SET_DRAGGED_ITEM': {
+      draft.draggedItem = action.payload;
+      break;
+    }
   }
 };
